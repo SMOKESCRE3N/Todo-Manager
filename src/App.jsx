93170f4e1604fabe497e769af16a5b666cd16f85
SMOKESCRE3N <dixitkartik1203@gmail.com>
2025-05-22 11:25:01 +0530

    bg image fixed

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ import TodoItem from './components/TodoItem';
 
    return (
     <TodoProvider value={{todos,addTodo,deleteTodo,updateTodo,toggleComplete}}>
-    <div className="bg-[#172842] min-h-screen bg-[url('/Todo-Manager/todolist.jpeg')]
+    <div className="bg-[#172842] min-h-screen bg-[url('/todolist.jpeg')]
  py-8">
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
